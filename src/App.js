d3cddead02243e1f2ec1d5ad9dd6f7a81daadf99
zzangsally <zzangsally@naver.com>
2022-07-14 14:50:31 +0900

    main

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Basket from "./pages/Basket";
+
+function App() {
+  return (
+    <BrowserRouter basename="comento-shop">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
